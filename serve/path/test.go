adbd564c46a20260ff0858e1b48e19459d7d3c48
megafyk <megafyk@gmail.com>
2023-02-25 22:46:54 +0700

    add sse response channel

--- serve/path/test.go
+++ serve/path/test.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func GetHomepage(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +33,7 @@ func GetTest(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusInternalServerError)
 }
 
-type CompletionPayload struct {
+type CompletionReq struct {
 	Model            string   `json:"model"`
 	Prompt           string   `json:"prompt"`
 	Temperature      float64  `json:"temperature"`
@@ -41,6 +42,23 @@ type CompletionPayload struct {
 	FrequencyPenalty int      `json:"frequency_penalty"`
 	PresencePenalty  float64  `json:"presence_penalty"`
 	Stop             []string `json:"stop"`
+}
+type CompletionResp struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Choices []struct {
+		Text         string `json:"text"`
+		Index        int    `json:"index"`
+		Logprobs     any    `json:"logprobs"`
+		FinishReason string `json:"finish_reason"`
+	} `json:"choices"`
+	Usage struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
 }
 
 type ChatPrompt struct {
@@ -56,14 +74,15 @@ func ChatWithGpt(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	payload := CompletionPayload{
+	payload := CompletionReq{
 		Model:            "text-davinci-003",
 		Prompt:           chatPrompt.Prompt,
 		Temperature:      1,
-		MaxTokens:        100,
+		MaxTokens:        1024,
 		TopP:             1,
 		FrequencyPenalty: 0,
 		PresencePenalty:  0,
+		//Stop:             []string{"\n"},
 	}
 
 	payloadBytes, err := json.Marshal(payload)
@@ -78,8 +97,9 @@ func ChatWithGpt(w http.ResponseWriter, r *http.Request) {
 	req.Header.Set("Authorization", os.ExpandEnv("Bearer "+os.Getenv("OPENAI_API_KEY")))
 
 	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Error().Msg(fmt.Sprintf("failed to do request %s", err))
+	if err != nil || res.StatusCode != 200 {
+		log.Error().Msg(fmt.Sprintf("failed to do request to openai %d %s", res.StatusCode, err))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -90,45 +110,68 @@ func ChatWithGpt(w http.ResponseWriter, r *http.Request) {
 		}
 	}(res.Body)
 
-	data, err := io.ReadAll(res.Body)
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	raw, err := io.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
-
-	log.Info().Msg(string(data))
-	_, err = io.WriteString(w, string(data))
+	log.Info().Msg(fmt.Sprintf("received %s", string(raw)))
+	var data CompletionResp
+	err = json.Unmarshal(raw, &data)
 	if err != nil {
-		log.Error().Msg("failed reply chat")
+		log.Error().Msg(fmt.Sprintf("failed parse response from openai %s", err))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	//msgChan := make(chan string)
-	//flusher, _ := w.(http.Flusher)
-	//
-	//defer func() {
-	//	close(msgChan)
-	//	msgChan = nil
-	//	log.Info().Msg("closed sse connection")
-	//}()
-	//
-	//for {
-	//	select {
-	//
-	//	// message will be received here and printed
-	//	case message := <-msgChan:
-	//		log.Info().Msg(message)
-	//		_, err := io.WriteString(w, message)
-	//		if err != nil {
-	//			log.Error().Msg("failed to response")
-	//			return
-	//		}
-	//		flusher.Flush()
-	//
-	//	// connection is closed then defer will be executed
-	//	case <-r.Context().Done():
-	//		log.Info().Msg("end chat reply")
-	//		return
-	//	}
-	//
-	//}
+	msgChan := make(chan string, 1024)
+	timeout := time.After(3 * time.Second)
+	flusher, _ := w.(http.Flusher)
+
+	defer func() {
+		close(msgChan)
+		msgChan = nil
+		log.Info().Msg("closed sse connection to server")
+	}()
+
+	// split to chunk -> send to channel
+	go func() {
+		text := data.Choices[0].Text
+		chunkSize := 3
+		for i := 0; i < len(text); i += chunkSize {
+			end := i + chunkSize
+			if end > len(text) {
+				end = len(text)
+			}
+			msgChan <- text[i:end]
+		}
+	}()
+
+	for {
+		select {
+
+		// message will be received here and printed
+		case message := <-msgChan:
+			log.Info().Msg(message)
+			_, err := io.WriteString(w, message)
+			if err != nil {
+				log.Error().Msg("failed to response")
+				return
+			}
+			time.Sleep(100 * time.Millisecond)
+			flusher.Flush()
+
+		// connection is closed then defer will be executed
+		case <-r.Context().Done():
+			log.Info().Msg("client end chat reply")
+			return
+		case <-timeout:
+			log.Info().Msg("end chat reply")
+			return
+		}
+	}
 }
